d59a2f23d44c26293cb083001fd4b350bd2b4c0d
iandemed <iandemed@gmail.com>
2020-11-06 22:37:51 -0500

    Added GET and POST requests for texture routes

--- db.py
+++ db.py
@@ -110,7 +110,7 @@ app = Flask(__name__)
 
 @app.route('/cheese/', methods=['GET', 'POST'])
 @app.route('/cheese/<id>', methods=['GET', 'PUT', 'DELETE'])
-def endpoint(id=None):
+def cheeses(id=None):
     print(request)
     if request.method == 'GET':
         if id:
@@ -124,6 +124,25 @@ def endpoint(id=None):
     if request.method == 'POST':
         new_cheese = dict_to_model(Cheese, request.get_json())
         new_cheese.save()
+        return jsonify({"success": True})
+
+
+@app.route('/texture', methods=['GET', 'POST'])
+@app.route('/texture/<id>', methods=['GET', 'PUT', 'DELETE'])
+def textures(id=None):
+    print(request)
+    if request.method == 'GET':
+        if id:
+            return jsonify(model_to_dict(Texture.get(Texture.id == id)))
+        else:
+            texture_list = []
+            for texture in Texture.select():
+                texture_list.append(model_to_dict(texture))
+            return jsonify(texture_list)
+
+    if request.method == 'POST':
+        new_texture = dict_to_model(Texture, request.get_json())
+        new_texture.save()
         return jsonify({"success": True})
 
 
