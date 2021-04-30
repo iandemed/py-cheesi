335e9744932609c1e11e245df9fdc7cef6057f65
Ian de Medeiros <44042532+iandemed@users.noreply.github.com>
2021-04-30 18:21:14 -0400

    Now able to scrape seed data using command with manage.py

--- manage.py
+++ manage.py
@@ -1,8 +1,10 @@
 import os
-from flask_script import Manager
+from flask_script import Manager, Command
 from flask_migrate import Migrate, MigrateCommand
 
 from app import create_app, db
+from db.scrape_seed_data import scrape_seed_data
+
 
 app = create_app()
 app.config.from_object(os.getenv('APP_SETTINGS'))
@@ -12,7 +14,14 @@ db.init_app(app)
 migrate = Migrate(app, db)
 manager = Manager(app)
 
+class ScrapeSeedData(Command):
+    "Scrapes cheese.com for data to populate the cheese API."
+
+    def run(self):
+        scrape_seed_data()
+
 manager.add_command('db', MigrateCommand)
+manager.add_command('seeddata', ScrapeSeedData())
 
 
 if __name__ == '__main__':
